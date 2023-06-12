fee63b9e47428733770f970fa748f028e907d43d
enokawa <enokawa00@gmail.com>
2023-06-12 17:45:10 +0900

    Re-initialize pipeline

--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -1,19 +1,16 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class PipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'PipelineQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    // The code that defines your stack goes here
 
-    const topic = new sns.Topic(this, 'PipelineTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // example resource
+    // const queue = new sqs.Queue(this, 'PipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
   }
 }
