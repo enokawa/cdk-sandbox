bdf8658e9cb5cb66676ce42daa79dadea37e7a42
enokawa <enokawa00@gmail.com>
2023-06-16 17:45:40 +0900

    cdk init

---
+++ pipeline-highlevel/test/pipeline-highlevel.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PipelineHighlevel from '../lib/pipeline-highlevel-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pipeline-highlevel-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PipelineHighlevel.PipelineHighlevelStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
