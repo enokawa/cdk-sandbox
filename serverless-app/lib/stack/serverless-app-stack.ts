d6d6916b68b255c369310a4a05a4df4b5e6ca08e
enokawa <enokawa00@gmail.com>
2024-01-04 12:04:01 +0900

    Add AWS WAF v2

--- serverless-app/lib/stack/serverless-app-stack.ts
+++ serverless-app/lib/stack/serverless-app-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Datastore } from '../construct/datastore';
+import { Waf } from '../construct/waf';
 
 import { Construct } from 'constructs';
 
@@ -8,5 +9,10 @@ export class ServerlessAppStack extends Stack {
     super(scope, id, props);
 
     new Datastore(this, 'Datastore');
+    new Waf(this, 'Waf', {
+      scope: 'REGIONAL',
+      ipAddresses: ['1.1.1.1/32'], // Enter your IP
+      defaultAction: { block: {} }
+    });
   }
 }
