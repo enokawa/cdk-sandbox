cde134f342ab6e88ae044ceca25b0429d065f316
enokawa <enokawa00@gmail.com>
2024-01-04 14:16:14 +0900

    Add Lambda

--- serverless-app/lib/stack/serverless-app-stack.ts
+++ serverless-app/lib/stack/serverless-app-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Datastore } from '../construct/datastore';
 import { Waf } from '../construct/waf';
+import { ApiFunction } from '../construct/lambda';
 
 import { Construct } from 'constructs';
 
@@ -8,11 +9,16 @@ export class ServerlessAppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Datastore(this, 'Datastore');
+    const table = new Datastore(this, 'Datastore');
+
     new Waf(this, 'Waf', {
       scope: 'REGIONAL',
       ipAddresses: ['1.1.1.1/32'], // Enter your IP
       defaultAction: { block: {} }
+    });
+
+    new ApiFunction(this, 'ApiFunction', {
+      table: table.table,
     });
   }
 }
