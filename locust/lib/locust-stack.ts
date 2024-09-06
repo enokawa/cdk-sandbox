76d6509a5a8ef5453a94dca24c3c8ea06e91798c
enokawa <enokawa00@gmail.com>
2024-09-07 00:15:33 +0900

    Create VPC and Docker image

--- locust/lib/locust-stack.ts
+++ locust/lib/locust-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { DockerImageAsset, Platform } from 'aws-cdk-lib/aws-ecr-assets';
+import path = require('path');
 
 export class LocustStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      restrictDefaultSecurityGroup: false
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LocustQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const asset = new DockerImageAsset(this, 'MyBuildImage', {
+      directory: path.join(__dirname, '../locust'),
+      platform: Platform.LINUX_ARM64
+    });
+
+    // TODO: Create ECS
   }
 }
