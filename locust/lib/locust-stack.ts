bb883b2bf03241d859ac88ecad7e00bf7f5b05de
enokawa <enokawa00@gmail.com>
2024-09-07 01:17:01 +0900

    Add ECS Cluster and Task definition

--- locust/lib/locust-stack.ts
+++ locust/lib/locust-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { aws_ec2 as ec2, aws_ecs as ecs } from 'aws-cdk-lib';
 import { DockerImageAsset, Platform } from 'aws-cdk-lib/aws-ecr-assets';
 import path = require('path');
 
@@ -18,6 +18,23 @@ export class LocustStack extends cdk.Stack {
       platform: Platform.LINUX_ARM64
     });
 
-    // TODO: Create ECS
+    const cluster = new ecs.Cluster(this, 'ECSCluster', { vpc });
+
+    const taskDefinition = new ecs.TaskDefinition(this, 'TD', {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: '256',
+      memoryMiB: '512'
+    });
+
+    taskDefinition.addContainer('TheContainer', {
+      image: ecs.ContainerImage.fromDockerImageAsset(asset),
+      portMappings: [
+        {
+          containerPort: 8089
+        }
+      ]
+    });
+
+    // TODO: Add ECS Service
   }
 }
